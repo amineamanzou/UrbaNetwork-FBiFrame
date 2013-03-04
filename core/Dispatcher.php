033161a22b589c99aff4d72a223e25ead014eb54
Amine Amanzou <amineamanzou@gmail.com>
2013-03-04 01:39:26 +0100

    Installing the error manager
    Have to fixe it because know all my pages have a 404 error

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -7,8 +7,24 @@ class Dispatcher {
         $this->request = new Request();
         Router::parse($this->request->url,$this->request);
         $controller = $this->loadController();
-        // action permettant d'appeller une action 
-        call_user_func_array(array($controller,$this->request->action), $this->request->params);
+        //Si l'action n'est pas défini dans le tableau des méthodes du controller
+        if (!is_array($this->request->action,  get_class_methods($controller))){
+            $this->error('Le controller '.$this->request->controller.
+                    ' n\'a pas de méthode '.$this->request->action);
+        }
+        else {
+            // action permettant d'appeller une action 
+            call_user_func_array(array($controller,$this->request->action), $this->request->params);
+            $controller->render($this->request->action);
+        }    
+    }
+    
+    function error($message){
+        header('HTTP/1.0 404 Not Found');
+        $controller = new Controller($this->request);
+        $controller->set('message',$message);
+        $controller->render('/errors/404'); 
+        
     }
     
     function loadController(){
