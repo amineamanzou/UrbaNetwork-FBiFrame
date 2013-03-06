90e50491acac24268d4a07dd8759db757f879845
Amine Amanzou <amineamanzou@gmail.com>
2013-03-06 21:10:27 +0100

    Adding the controller of each pages
    Using some data in the DB for test
    cf --> shemacreate.sql file

--- controller/PagesController.php
+++ controller/PagesController.php
@@ -1,15 +1,52 @@
 <?php
 class PagesController extends Controller{
     
-    function view($nom){
-        $this->loadModel('Wids');
-        $this->set(array(
-           'phrase' => 'Salut',
-            'nom' => 'Mr. Visiteur'
+    function WhatTheyDo(){
+        $this->loadModel('Wid');
+        
+        $d['page'] = $this->Wid->findfriendwids(array(
+            1112232131,
+            12434352433,
+            1123142131
         ));
-        $this->render($nom);
+        $d['pages'] = get_class_methods($this);
+        if(empty($d['page'])){
+            $this->e404('Page introuvable');
+        }
+        
+        $this->set($d);
     }
     
+    function WhatIDo(){
+        $this->loadModel('Wid');
+        
+        $d['page'] = $this->Wid->finduserwids(array(
+            1112232131
+        ));
+        $d['pages'] = get_class_methods($this);
+
+        if(empty($d['page'])){
+            $this->e404('Page introuvable');
+        }
+        
+        $this->set($d);
+    }
+    
+    function Profile(){
+        
+    }
+    
+    function Channel(){
+        
+    }
+    
+    function Contact(){
+        
+    }
+    
+    function Company(){
+        
+    }
 }
 ?>
  
