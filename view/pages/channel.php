124192a6064c66a50c3b2823862e14101a5f14f5
Amine Amanzou <amineamanzou@gmail.com>
2013-03-09 22:27:54 +0100

    Updating the channel view

--- view/pages/channel.php
+++ view/pages/channel.php
@@ -9,13 +9,39 @@
     </div>
 </div>
 
-<div>
-    Ma Suite de thumbnail 10 derniere video uploadé avec bouton +1
+<div class="container canvas">
+    <div class="row">
+        <?php foreach($videoFeed as $v): $thumbs = $v->getVideoThumbnails(); ?>
+        <div class="span4" style="height:300px;">
+            <div class="thumbnail">
+                <h3><?php echo $v->getVideoTitle(); ?></h3>
+                <img class="thumbnail" src="<?php echo $thumbs[2]['url'];  ?>" >
+                <p><?php echo $v->getVideoDescription(); ?></p>
+                          <div id="modal-from-dom" class="modal hide fade">
+            <div class="modal-header">
+              <a href="#" class="close">&times;</a>
+              <h3><?php echo $v->getVideoTitle(); ?></h3>
+            </div>
+            <div class="modal-body">
+              <p><iframe width="500" height="281" src="<?php echo $v->getFlashPlayerUrl(); ?>" frameborder="0" allowfullscreen></iframe></p>
+            </div>
+            <div class="modal-footer">
+              <a href="#" class="btn primary">Vote for this video</a>
+              <a href="#" class="btn secondary">Share</a>
+            </div>
+          </div>
+        <button data-controls-modal="modal-from-dom" data-backdrop="true" data-keyboard="true" class="btn primary">Voir cette vidéo</button>
+                <p>
+                    <a href="<?php echo $v->getVideoWatchPageUrl(); ?>" class="btn danger">Voir sur youtube</a>
+                    <span class="label notice"><?php echo round($v->getVideoDuration()/60); ?>min</span>
+                </p>
+                
+                
+            </div>
+        </div>
+        <?php endforeach;?>
+    </div>
 </div>
 
-OR
 
-<div>
-    Formulaire génerer channel
-</div>
 
