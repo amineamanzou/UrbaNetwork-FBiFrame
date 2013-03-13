415ef40bae9017af36a539465e1af69b5379d739
Amine Amanzou <amineamanzou@gmail.com>
2013-03-13 12:20:14 +0100

    Adding information about the project on some views

--- view/pages/company.php
+++ view/pages/company.php
@@ -1,3 +1,22 @@
-<?php session_start(); ?>
-Information sur le projet, open source lien github présence sur twitter.
-<?php debug($_SESSION['user']); ?>
+<div class="hero-unit">
+    <h2>About the company</h2>
+    <h4><strong>This application was developped by : Amanzou Amine.</strong><h4>
+</div>
+<div class="row">
+
+    <div class="span6">
+        <center><img src="http://fredods.com/wp-content/uploads/2012/08/github.png"/></center>
+    </div>
+    <div class="span6">
+        <br><br><br><br>
+            <p>
+            <h4>It's a part of a bigger open source project that we have done in university.<br>
+                You can discover more about it <a href="https://github.com/amineamanzou/UrbaNetwork"> here </a>.<br>
+                We love open source, web developpement and urbane networks.<br><br>
+                <strong>Go put your Artistic CV on our website ;).</strong><br></h4>
+            </p>
+    </div>
+
+</div>
+ 
+
--- view/pages/contact.php
+++ view/pages/contact.php
@@ -1,2 +1,56 @@
-<?php session_start(); ?>
-Formulaire de contact
+<div class="hero-unit">
+        <h2><strong>Stay in touch with us.</strong></h2>
+        <p class="tagline">
+        <h4>Thanks testing our Facebook application. Don't hesitate to contact our team developement filling thos information.</h4>
+        </p>  
+</div><!--end hero unit -->
+
+<div class="row">
+    <div class="span4">
+        <form>
+            <div class="control-group">
+                <label class="control-label" for="inputEmail">Nom et Prénom</label>
+                <div class="controls controls-row">
+                    <input type="text" class="input-xxlarge" id="inputEmail" placeholder="Personne Cool">
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="inputEmail">Email</label>
+                <div class="controls">
+                    <input type="text" class="input-xxlarge" id="inputEmail" placeholder="nomprenom@exemple.com">
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="inputEmail">Objet</label>
+                <div class="controls">
+                    <input type="text" class="input-xxlarge" id="inputSubject" placeholder="Comment allez vous ?">
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="inputEmail">Message</label>
+                <div class="controls">
+                    <textarea rows="6" class="input-xxlarge" placeholder=""></textarea>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-success"> Envoyer </button>
+                </div>
+            </div>
+        </form>
+        <br class="clear">
+    </div><!-- end left -->
+    <div class="span2">Form</div>
+    <div class="span6">
+        <div class="well">
+            <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+                    src="https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=new+york+city&aq=&sll=37.6,-95.665&sspn=80.867771,100.263672&t=m&ie=UTF8&hq=&hnear=Fontainebleau&z=11&iwloc=A&output=embed">
+            </iframe>
+        </div><!--end well -->
+    </div><!--end right -->
+
+</div><!--end row -->
