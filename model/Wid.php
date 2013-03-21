b649fc32109368cec8f729b4f76281daafa874b6
Amine Amanzou <amineamanzou@gmail.com>
2013-03-21 09:43:28 +0100

    Updating model Wid

--- model/Wid.php
+++ model/Wid.php
@@ -13,7 +13,7 @@ class Wid extends Model{
        
     }
     
-    public function findfriendwids($req){
+    public function findPublicWids($req = Null){
         $sql = 'SELECT nom, prenom, verbe, place, quand FROM '.$this->table.' as '. get_class($this) .' NATURAL JOIN postes NATURAL JOIN users ';
         if (isset($req[0])){
             $sql .= 'WHERE facebook_id = '.$req[0].' ';
@@ -29,16 +29,30 @@ class Wid extends Model{
         return $pre->fetchAll(PDO::FETCH_OBJ);
     }
     
-    public function finduserwids($req){
+    public function findUserWids($req){
         $sql = 'SELECT nom, prenom, verbe, place, quand FROM '.$this->table.' as '. get_class($this) .' NATURAL JOIN postes NATURAL JOIN users ';
-        if (isset($req[0])){
-            $sql .= 'WHERE facebook_id = '.$req[0].' ';
+        if (isset($req['conditions'])){
+            $sql .= 'WHERE facebook_id = '.$req['conditions'].' ';
         }
-        $sql .= 'ORDER BY quand DESC LIMIT 10;';
+        $sql .= 'ORDER BY quand DESC ';
+        if (isset($req['limit'])){
+            $sql .= 'LIMIT '.$req['limit'];
+        }
         $pre = $this->db->prepare($sql);
         $pre->execute();
         return $pre->fetchAll(PDO::FETCH_OBJ);
     } 
+    
+    public function findCount($req){
+        $sql = 'SELECT COUNT(wids_id) as count FROM '.$this->table.' as '. get_class($this) .' NATURAL JOIN postes NATURAL JOIN users ';
+        if(isset($req['conditions'])){
+            $sql .= 'WHERE facebook_id = '.$req['conditions'].' ';
+        }
+        $pre = $this->db->prepare($sql);
+        $pre->execute();
+        $res = current($pre->fetchAll(PDO::FETCH_OBJ));
+        return $res->count;
+    }
     
 }
 ?>
